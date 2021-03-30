85c3b3ca9bd10739b05918c0c8d8404b664866eb
Thomas Mol <email@thomasmol.com>
2021-03-30 15:15:39 +0200

    Get ranking

--- lib/tabs/ranking_tab.dart
+++ lib/tabs/ranking_tab.dart
@@ -1,37 +1,73 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import '../navigation_bar.dart';
+import '../api.dart';
+import '../widgets.dart';
 
 class RankingTab extends StatefulWidget {
   static const title = 'Ranking';
-  static const icon = Icon(CupertinoIcons.star_fill);
+  static const icon = Icon(CupertinoIcons.list_number);
 
   @override
   _RankingTabState createState() => _RankingTabState();
 }
 
-class _RankingTabState extends State<RankingTab>{
+class _RankingTabState extends State<RankingTab> {
+  Future<List<dynamic>> futureRanking;
+
   @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        NavigationBar(),
-        CupertinoSliverRefreshControl(
-            //onRefresh: _refreshData,
-            ),
-        /* SliverSafeArea(
-          top: false,
-          sliver: SliverPadding(
-            padding: EdgeInsets.symmetric(vertical: 12),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                _listBuilder,
-                childCount: _itemsLength,
-              ),
-            ),
-          ),
-        ), */
-      ],
-    );
+  void initState() {
+    super.initState();
+    futureRanking = fetchOrganizationRanking();
   }
 
+  Future<List<dynamic>> fetchOrganizationRanking() async {
+    var response = await CallApi().getRequest(null, '/toptenranking');
+    if (response['status'] == 'Success') {
+      return response['data'];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final rankingBuilder = FutureBuilder<List<dynamic>>(
+        future: futureRanking,
+        builder: (context, snapshot) {
+          Widget newsListSliver;
+          if (snapshot.hasData) {
+            newsListSliver = SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+              int position = index + 1;
+              return ListTile(
+                leading: Text(position.toString()),
+                title: Text(snapshot.data[index]['name']),
+                trailing: Text(snapshot.data[index]['score'].toString()),
+              );
+            }, childCount: snapshot.data.length));
+          } else {
+            newsListSliver = SliverToBoxAdapter(
+              child: CupertinoActivityIndicator(),
+            );
+          }
+          return newsListSliver;
+        });
+
+    return SafeArea(
+        child: Scaffold(
+            body: CustomScrollView(
+      slivers: [
+        NavigationBar(),
+        CupertinoSliverRefreshControl(onRefresh: () async {
+          reloadData();
+        }),
+        rankingBuilder
+      ],
+    )));
+  }
+
+  void reloadData() {
+    setState(() {
+      futureRanking = fetchOrganizationRanking();
+    });
+  }
 }
